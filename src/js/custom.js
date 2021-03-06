710cd3ec7a1efbe82f31113501328eb6e9c08552
Jan Heder <info@janheder.cz>
2021-03-06 18:38:35 +0100

    css

--- src/js/custom.js
+++ src/js/custom.js
@@ -4,7 +4,7 @@
 
 
 
-$('<div class="hp-blog"><div class="container"><div class="news-wrapper"></div></div></div>').insertBefore("#footer");
+$('<div class="hp-blog"><div class="container"><div class="news-wrapper"></div></div></div>').insertBefore(".in-index #footer");
 $(".hp-blog .news-wrapper").load("/blog/ .news-wrapper .news-item:nth-child(-n+3)");
 
 // =============================================================================
