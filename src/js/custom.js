4baf7e5a1316b65e8f6e10ac21b76d390decc7f9
Jan Heder <info@janheder.cz>
2021-03-08 18:07:01 +0100

    js

--- src/js/custom.js
+++ src/js/custom.js
@@ -7,30 +7,7 @@
 $('<div class="hp-blog"><div class="container"><div class="news-wrapper"></div></div></div>').insertBefore(".in-index #footer");
 $(".hp-blog .news-wrapper").load("/blog/ .news-wrapper .news-item:nth-child(-n+3)");
 
-// =============================================================================
-// LOAD INSTAGRAM FEED
-// =============================================================================
 
-(function($){
-    $(window).on('load', function(){
-        $.instagramFeed({
-            'username': 'jan.heder',
-            'container': "#instagramSectionContent",
-            'display_profile': false,
-            'display_biography': false,
-            'display_gallery': true,
-            'get_data': false,
-            'callback': null,
-            'styling': true,
-            'items': 8,
-            'items_per_row': 8,
-            'image_size': 150,
-            'margin': 0.5,
-            'lazy_load': true,
-            'on_error': console.error
-        });
-    });
-})(jQuery);
 
 
 // =============================================================================
