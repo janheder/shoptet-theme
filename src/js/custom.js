61dce0d814de1bde7f540961be80af70dd67ae7b
Jan Heder <info@janheder.cz>
2022-05-11 21:41:14 +0200

    .

--- src/js/custom.js
+++ src/js/custom.js
@@ -2,8 +2,8 @@
 // INIT HOMEPAGE CAROUSEL
 // =============================================================================
 $('<div class="swiffy-slider slider-nav-arrow slider-nav-autoplay slider-nav-autopause slider-indicators-round slider-nav-mousedrag" data-slider-nav-autoplay-interval="5000"><ul class="slider-container"></ul><button type="button" class="slider-nav"></button> <button type="button" class="slider-nav slider-nav-next"></button></div>').insertBefore(".in-index #content-wrapper");
-$('#carousel img').each(function(){
-     $(".swiffy-slider .slider-container").append('<li><div class="slide-content"><h1><span>' + $(this).attr("alt") + '</span></h1></div><div class="slide-img"><img src="' + $(this).attr("src") + '"></div></li>');
+$('#carousel .item').each(function(){
+     $(".swiffy-slider .slider-container").append('<li><div class="slide-content"><h1><span>' + $(this).find("img").attr("alt") + '</span></h1><a href="' + $(this).find("a").attr("href") + '" class="btn">Více informací</a></div><div class="slide-img"><img src="' + $(this).find("img").attr("src") + '"></div></li>');
 });
 $(".swiffy-slider .slider-container li:first-child img").addClass("active");
 
