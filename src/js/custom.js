5fec3ed67ade5bd70aed9b2460564e694051c892
Jan Heder <info@janheder.cz>
2022-06-01 23:52:53 +0200

    .

--- src/js/custom.js
+++ src/js/custom.js
@@ -26,9 +26,10 @@ $(".p .p-tools form").prepend('<span class="quantity"><input type="text" value="
 // =============================================================================
 
 $(".in-index .products.products-block").each(function() {
+    $(this).data("slider-nav-autoplay-interval","8000");
+
     if($(this).find('.product').length > 4){
-        $(this).addClass("swiffy-slider slider-item-show4 slider-nav-arrow slider-nav-autoplay slider-nav-autopause slider-indicators-round");
-        $(this).data("slider-nav-autoplay-interval","5000");
+        $(this).addClass("swiffy-slider slider-item-show4 slider-nav-arrow slider-nav-autoplay slider-nav-autopause slider-indicators-round slider-nav-page");
         $(this).wrapInner("<div class='slider-container'></div>");
         $(this).append('<button type="button" class="slider-nav"></button> <button type="button" class="slider-nav slider-nav-next"></button>');
     };
