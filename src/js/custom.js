d2b05a2ea4b5e6cd723c6a8474eff4436dd339ad
Jan Heder <info@janheder.cz>
2022-06-28 20:09:51 +0200

    .

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,3 +1,49 @@
+// -----------------------------------------------------------------------------
+// GLOBAL
+// -----------------------------------------------------------------------------
+
+/* remove native shoptet scroll function - bugs */
+if ($("[data-force-scroll]").length){
+    $("[data-force-scroll]").each(function(){
+        $(this).removeAttr("data-force-scroll");
+    });
+}
+
+/* anchor scroll setup */
+$(document).ready(function() {
+    $('a[href*="#"]').click(function(event) {
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+          && 
+          location.hostname == this.hostname
+        ) {
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000, function() {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              };
+            });
+          }
+        }
+    });
+});
+
+
 // =============================================================================
 // INIT HOMEPAGE CAROUSEL
 // =============================================================================
